568f9096ab2b806d19cfcbf9d524354bf75abaf8
Rusiek <rusiekae86@gmail.com>
2021-10-29 13:56:21 +0200

    '26. Remove Duplicates from Sorted Array' easy solution

---
+++ Tasks 1-50/0026_Remove_Duplicats_fSA/cmake-build-debug/CMakeFiles/0026_Remove_Duplicats_fSA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0026_Remove_Duplicats_fSA.exe"
+  "0026_Remove_Duplicats_fSA.exe.manifest"
+  "0026_Remove_Duplicats_fSA.pdb"
+  "CMakeFiles/0026_Remove_Duplicats_fSA.dir/main.cpp.obj"
+  "lib0026_Remove_Duplicats_fSA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0026_Remove_Duplicats_fSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
