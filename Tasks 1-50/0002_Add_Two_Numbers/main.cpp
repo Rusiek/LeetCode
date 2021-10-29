c3720cffe57d41f479b620d742ae5451dba0ad49
Rusiek <rusiekae86@gmail.com>
2021-10-29 03:34:00 +0200

    '2. Add Two Numbers' recursion solution

--- Tasks 1-50/0002_Add_Two_Numbers/main.cpp
+++ Tasks 1-50/0002_Add_Two_Numbers/main.cpp
@@ -1,16 +1,5 @@
 #include <bits/stdc++.h>
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 struct ListNode{
     int val;
     ListNode *next;
@@ -54,22 +43,3 @@ public:
         return output;
     }
 };
-
-int main() {
-
-    ListNode A(4), B(2), C(6), D(9);
-    ListNode X(2), Y(1), Z(8);
-
-    A.next = &B;
-    B.next = &C;
-    C.next = &D;
-
-    X.next = &Y;
-    Y.next = &Z;
-
-    Solution ABC;
-    ABC.addTwoNumbers(&A, &X);
-
-
-    return 0;
-}
