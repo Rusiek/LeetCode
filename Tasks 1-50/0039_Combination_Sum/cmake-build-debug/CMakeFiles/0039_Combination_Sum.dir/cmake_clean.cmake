7dda597d9a9de12e39b2ad7239ba8a656994add0
Rusiek <rusiekae86@gmail.com>
2021-10-30 14:16:42 +0200

    '39. Combination Sum' Dynamic method solution

---
+++ Tasks 1-50/0039_Combination_Sum/cmake-build-debug/CMakeFiles/0039_Combination_Sum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0039_Combination_Sum.exe"
+  "0039_Combination_Sum.exe.manifest"
+  "0039_Combination_Sum.pdb"
+  "CMakeFiles/0039_Combination_Sum.dir/main.cpp.obj"
+  "lib0039_Combination_Sum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0039_Combination_Sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
