d3d23f7b74cb61ca221ff830fa4cb116b59d4a88
Rusiek <rusiekae86@gmail.com>
2021-10-31 16:57:37 +0100

    '28. Implement strStr()' fast bruteforce solution

---
+++ Tasks 1-50/0028_Implement_strStr/cmake-build-debug/CMakeFiles/0028_Implement_strStr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0028_Implement_strStr.exe"
+  "0028_Implement_strStr.exe.manifest"
+  "0028_Implement_strStr.pdb"
+  "CMakeFiles/0028_Implement_strStr.dir/main.cpp.obj"
+  "lib0028_Implement_strStr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0028_Implement_strStr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
