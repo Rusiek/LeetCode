0a00eb629da41863c1ffc98f4824cba3c0fc0ce1
Rusiek <rusiekae86@gmail.com>
2021-10-31 23:06:16 +0100

    '12. Integer to Roman' easy solution

---
+++ Tasks 1-50/0012_Integer_to_Roman/cmake-build-debug/CMakeFiles/0012_Integer_to_Roman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0012_Integer_to_Roman.exe"
+  "0012_Integer_to_Roman.exe.manifest"
+  "0012_Integer_to_Roman.pdb"
+  "CMakeFiles/0012_Integer_to_Roman.dir/main.cpp.obj"
+  "lib0012_Integer_to_Roman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0012_Integer_to_Roman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
