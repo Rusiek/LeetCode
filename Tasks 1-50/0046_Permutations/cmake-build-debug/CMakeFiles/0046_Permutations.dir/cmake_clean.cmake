c5ec2abf9f8bc908445b8e99eb484c658202d8f3
Rusiek <rusiekae86@gmail.com>
2021-10-31 22:57:00 +0100

    '46. Permutations' recursion solution

---
+++ Tasks 1-50/0046_Permutations/cmake-build-debug/CMakeFiles/0046_Permutations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0046_Permutations.exe"
+  "0046_Permutations.exe.manifest"
+  "0046_Permutations.pdb"
+  "CMakeFiles/0046_Permutations.dir/main.cpp.obj"
+  "lib0046_Permutations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0046_Permutations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
