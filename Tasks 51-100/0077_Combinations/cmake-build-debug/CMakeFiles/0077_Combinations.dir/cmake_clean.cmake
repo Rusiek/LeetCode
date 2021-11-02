b472d8f421f49690572db66d25a03921178baf0f
Rusiek <rusiekae86@gmail.com>
2021-11-02 02:31:44 +0100

    '77. Combinations' Backtracking method solution

---
+++ Tasks 51-100/0077_Combinations/cmake-build-debug/CMakeFiles/0077_Combinations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0077_Combinations.exe"
+  "0077_Combinations.exe.manifest"
+  "0077_Combinations.pdb"
+  "CMakeFiles/0077_Combinations.dir/main.cpp.obj"
+  "lib0077_Combinations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0077_Combinations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
